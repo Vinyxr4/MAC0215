8d51e372f571c72292b4b5668daa5a8ad4e7e2b6
vinyxr4 <viniciuspessoaduarte@gmail.com>
2017-09-03 21:23:28 -0300

    PDF extractor API

--- MAC0215/text.h
+++ MAC0215/text.h
@@ -18,11 +18,9 @@ class text {
 public:
     text(QString font);
 
-    void LoadCube();
     void define_font_type (QString font);
     void define_text (QString t, std::vector<QVector3D> quad_vertices);
     void bake_atlas ();
-    void render_text ();
     void define_text_from_pdf (QString pdf_path);
     void gen_test ();
     void gen_test_pdf ();
